a99acadee08b95e7e354f6ca610866c9c3ac3633
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-07-30 19:49:25 -0700

    added boilerplate code for accordion

---
+++ client/src/components/AccordionComponent.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+export const AccordionComponent = () => {
+
+    //Video Presentation
+    //Brain Integration Training
+    //Clinical Hours
+    //First Aid Certification
+    //CPR Certification
+    //Insurance
+    //Study Guide
+    //Assessment 
+    //If you set allowMultipleExpanded to true then the accordion will permit multiple items to be expanded at once.
+    //<Accordion allowMultipleExpanded>
+    //import items as props or context and map over them
+
+    return (
+        <div>
+            <p>
+            Follow these steps to submit your documentation for certification review.  You may complete them in any order, except for the assessment, which can only be accessed once the preceding items are completed.  If you have any questions about the process, please contact us, and a member of our board will be happy to assist you.
+            </p>
+        </div>
+
+    )
+}
