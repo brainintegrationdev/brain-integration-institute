e21fec0757ff91b4decdd92ae15453d17f0e8c9b
valerie smith <valerie.plante.11@gmail.com>
2024-08-05 22:43:58 -0400

    practitioner card done

--- client/src/routes/Practitioner.jsx
+++ client/src/routes/Practitioner.jsx
@@ -1,7 +1,31 @@
+import React, {useContext} from "react";
+import { PractitionerCard } from "../components/PractitionerCard"
+import { PractitionerContext } from "../contexts";
+
+
 export const Practitioner = () => {
+
+const {practitioners} = useContext(PractitionerContext)
+
+const practitionerList = practitioners.map(person => 
+    <PractitionerCard 
+    firstName = {person.firstName}
+    lastName = {person.lastName}
+    title = {person.title}
+    location = {person.location}
+    imgURL = {person.imgURL}
+    phone = {person.phone}
+    />
+)
+
+
+
     return (
         <>
         <h1>Find Practitioner</h1>
+        <div style = {{display:"inline-block"}}>
+        {practitionerList}
+        </div>
         </>
     )
 }
