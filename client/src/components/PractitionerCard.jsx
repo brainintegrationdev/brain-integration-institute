974f70c1db85bf17402d1ab620a3e5eb46698bef
valerie smith <valerie.plante.11@gmail.com>
2024-08-05 22:53:29 -0400

    last commit before PR request

--- client/src/components/PractitionerCard.jsx
+++ client/src/components/PractitionerCard.jsx
@@ -6,12 +6,9 @@ export const PractitionerCard = (props) => {
 
 const {firstName, lastName, title, location, imgURL, phone} = props
 
-// console.log(practitioners)
-
 
     return(
-        <div style = {{    display: 'inline-block', width: '38.375rem', height: '17.5rem',margin : "0.5rem", padding: '1.5rem', flexDirection : "column", alignItems: 'flex-start', gap: 'var(--sds-size-space-200)', flexShrink: 0, borderRadius: '0.25rem', opacity: 'var(--sds-size-stroke-border)', background: '#ECECEC', boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}}>
-
+        <div style = {{ display: 'inline-block', width: '38.375rem', height: '17.5rem',margin : "0.5rem", padding: '1.5rem', flexDirection : "column", alignItems: 'flex-start', gap: 'var(--sds-size-space-200)', flexShrink: 0, borderRadius: '0.25rem', opacity: 'var(--sds-size-stroke-border)', background: '#ECECEC', boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'}}>
                 <div style = {{display: 'flex', alignItems: 'flex-start', gap: '2.5rem'}}>
             <img style = {{width: '11.75rem',height: '11.75rem',borderRadius: '0.25rem',opacity: 'var(--sds-size-stroke-border)',objectFit : "cover"}}src = {imgURL}/>
        <div>
